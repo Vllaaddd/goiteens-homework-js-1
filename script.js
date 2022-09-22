5d3a532f8cba8af2c70c613edd7e553b6c1c1691
Vlad <vladikhoncharuk@gmail.com>
2022-09-22 18:40:52 +0300

    complete homework

---
+++ script.js
@@ -0,0 +1,69 @@
+let str = 'Привіт';
+let num = 123;
+let flag = true;
+let txt = 'true';
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);
+
+
+let firstName = 'Tom';
+let lastName = 'Jack';
+let age = 30;
+let text = 'asdfghjkl';
+let trueFalse = true;
+
+const nameFirst = 'Tom';
+const nameLast = 'Jack';
+const Age = 40;
+const Text = 'asdfghjkl';
+const falseTrue = false;
+
+
+const myAge = 14;
+const friendAge = 14;
+
+alert(myAge + friendAge);
+
+let a = 10;
+let b = 20;
+console.log(a+b);
+
+let a1 = 5 + 3;
+let a2 = 5 - 3;
+let a3 = 5 * 3;
+let a4 = 5 / 3;
+let a5 = 5 + 5;
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
+
+let a6 = 5 % 3;
+let a7 = 3 % 5;
+let a8 = 5 + '3';
+let a9 = '5' - 3;
+let a10 = 75 + 'кг';
+
+console.log(a6);
+console.log(a7);
+console.log(a8);
+console.log(a9);
+console.log(a10);
+
+
+let height = 23;
+let width = 10;
+let s = height * width;
+
+let heightC = 10;
+let dC = 4;
+let v = heightC * dC;
+
+let n = 3;
+let m = 4;
+let k = (n**2) + (m**2);
